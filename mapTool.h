42ac45ee2be91faed81f23f136aeee3e3f668655
LeeJiu <leeji0408@skhu.kr>
2016-04-30 17:12:18 +0900

    first commit
    default

---
+++ mapTool.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "gameNode.h"
+
+
+class mapTool : public gameNode
+{
+private:
+
+
+public:
+	mapTool();
+	~mapTool();
+
+	HRESULT init();
+	void release();
+	void update();
+	void render();
+
+	//=====================추가할 함수=====================
+
+};
+
