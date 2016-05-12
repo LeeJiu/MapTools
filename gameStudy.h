8a64819d4a22088cf91a75f1eac98475d908a44b
JaeJun Kim <redforce01@naver.com>
2016-05-12 21:00:20 +0900

    backup
    backup

--- gameStudy.h
+++ gameStudy.h
@@ -1,24 +1,16 @@
 #pragma once
 
 #include "gameNode.h"
-<<<<<<< HEAD
 #include "selectStage.h"
 #include "battleScene.h"
 #include "town.h"
-=======
 #include "mapTool.h"
 #include "title.h"
 #include "aStar.h"
->>>>>>> refs/remotes/origin/development
 
 class gameStudy : public gameNode
 {
 private:
-<<<<<<< HEAD
-
-=======
->>>>>>> refs/remotes/origin/development
-
 public:
 	gameStudy();
 	virtual ~gameStudy();
