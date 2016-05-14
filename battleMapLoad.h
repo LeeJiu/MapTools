b544e18311b438aa44fd47b92540d223e549cca2
JaeJun Kim <redforce01@naver.com>
2016-05-14 16:35:13 +0900

    update_160514_CAFE :o
    update_160514_CAFE :o

--- battleMapLoad.h
+++ battleMapLoad.h
@@ -33,6 +33,6 @@ public:
 	void render();
 
 	void loadMapData();
-	vector<TagTile> getTileVector() { return _vTile; }
+	vector<TagTile> getTileVector() { return _vTile; }	
 };
 
--- battleScene.cpp
+++ battleScene.cpp
@@ -22,6 +22,7 @@ HRESULT battleScene::init()
 	_aStar = new aStar;
 	//_aStar->init(_battleMapLoad->getTileVector());
 
+	
 	return S_OK;
 }
 
