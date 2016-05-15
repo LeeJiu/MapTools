2bb727e3c94eff971b4b2dc49c2a7eaccf93d236
LeeJiu <leeji0408@skhu.kr>
2016-05-15 16:24:23 +0900

    add vector<string> mercenay into gameObject

--- Flonne.h
+++ Flonne.h
@@ -1,15 +1,15 @@
 #pragma once
 #include "character.h"
-class Flonne : public character
+class flonne : public character
 {
 public:
-	Flonne();
-	~Flonne();
+	flonne();
+	~flonne();
 
-	HRESULT init();
-	void release();
-	void update();
-	void render();
+	virtual HRESULT init();
+	virtual void release();
+	virtual void update();
+	virtual void render();
 
 	/*			함수 추가			*/
 	virtual void keyControl();
@@ -20,5 +20,7 @@ public:
 
 	virtual void saveData();
 	virtual void loadData();
+
+	virtual void setItem(const char* itemName);
 };
 
