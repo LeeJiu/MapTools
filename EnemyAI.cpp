7c3919ce1a7743121d123d3a40c94631530a8cb2
재준 김 <redforce01@naver.com>
2016-05-21 02:26:55 +0900

    EnemyAI Backup
    EnemyAI Backup

--- EnemyAI.cpp
+++ EnemyAI.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "EnemyAI.h"
-
+#include "battleManager.h"
+#include "gameObjectManager.h"
 
 EnemyAI::EnemyAI()
 {
@@ -8,5 +9,32 @@ EnemyAI::EnemyAI()
 
 
 EnemyAI::~EnemyAI()
+{
+}
+
+HRESULT EnemyAI::init()
+{
+	_isDoOrderAction = false;
+
+
+	return S_OK;
+}
+
+void EnemyAI::release()
+{
+}
+
+void EnemyAI::update()
+{
+	if (_orderCount < _gameObjMgr->getEnmSize())
+	{
+		_isDoOrderAction = false;
+		return;
+	}
+
+	
+}
+
+void EnemyAI::render()
 {
 }
